c0d74825a7f8b5d1000826d835d8ef49c811d630
Lucas Heber <lucas.heber07@gmail.com>
2019-12-01 16:05:54 -0300

    [add] testparm, [up] verifiando serviços

--- script.sh
+++ script.sh
@@ -25,20 +25,62 @@ sambaUser="root"
 
 verificando_servicos()
 {
+    passSamba="One8399*"
+    sambaServer="192.168.10.10"
+    sambaUser="root"
+
+    pass="One8399*"
+    proxyServer="192.168.11.11"
+    proxyUser="root"
+
+    work=$1
+    usersmb=$2
+    passuser=$3
+
+    RED='\033[0;31m'
+    YELLOW='\033[1;33m'
+    NC='\033[0m' # No Color  
+
     # Configurações do samba
-    smbclient="smbclient -W $workgroup -L 127.0.0.1 -U $usersmb%$passuser"
+    smbclient="smbclient -W $work -L 127.0.0.1 -U $usersmb%$passuser"
     smbactive="systemctl is-active smb nmb" # Verificando se os servicos Samba estao ativos
     smbenable="systemctl is-enabled smb nmb" # Verificando se os servicos Samba estao ativos
 
     smbclientSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbclient""
     smbcliuser=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$smbclientSSH | grep $usersmb"`
 
-    smbclientSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbclient""
-    smbcliuser=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$smbclientSSH | grep $usersmb"`
+    # Verificando se o smb e nmb estao ativos
+    sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbactive""
+    smbactstatus=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH"`
 
-    smbclientSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbclient""
-    smbcliuser=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$smbclientSSH | grep $usersmb"`
+    # Verificando se o smb e nmb estao "enabled"
+    sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbenable""
+    smbenastatus=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH"`
 
+    result=`echo "$smbcliuser" | egrep "$NT_STATUS_LOGON_FAILURE|$NT_STATUS_CONNECTION_REFUSED" `
+    if [ "$result" != "" ]; then
+        echo -e "${RED}[ERROR]${NC} O usuario $usersmb nao conectou ao serviço smb"
+    fi
+
+    result=`echo "$smbactstatus" | sed -n '1p'`
+    if [ "$result" == "inactive" ]; then
+        echo -e "${YELLOW}[WARNING]${NC} O Serviço smb está inativo!"
+    fi
+
+    result=`echo "$smbactstatus" | sed -n '2p'`
+    if [ "$result" == "inactive" ]; then
+        echo -e "${YELLOW}[WARNING]${NC} O Serviço nmb está inativo!"
+    fi
+
+    result=`echo "$smbenastatus" | sed -n '1p'`
+    if [ "$result" == "disaled" ]; then
+        echo -e "${YELLOW}[WARNING]${NC} O Serviço smb nao está ativado para iniciar automaticamente!"
+    fi
+
+    result=`echo "$smbenastatus" | sed -n '2p'`
+    if [ "$result" == "disaled" ]; then
+        echo -e "${YELLOW}[WARNING]${NC} O Serviço nmb nao está ativado para iniciar automaticamente!"
+    fi
 }
 # ################################################################################### # 
 #                          PEGANDO OS DADOS NECESSARIOS                               #
@@ -60,16 +102,10 @@ smbclientSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$
 # Acessando o proxy e obtendo o resultado do $testparm do servidor samba
 ## sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH" > $arquivoTeste 2> /dev/null
 
-# Verificando se o smb e nmb estao ativos
-sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbactive""
-##smbactstatus=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH"`
 
-# Verificando se o smb e nmb estao "enabled"
-sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbenable""
-##smbenastatus=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH"`
 
 # Pegando somente a primeira linha
-# echo "$smbenastatus" | sed -n '1p'
+
 
 users="cat /etc/passwd | egrep 'pedrito|palito|palhaco'"
 ##userslinux=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$users"`
@@ -98,3 +134,5 @@ pathnetlogon=`cat $arquivoTeste | grep -A5 "\[netlogon\]" | grep -m1 "path" | aw
 crlf=`file $pathnetlogon/$logonscript | awk -F: '{ print $2}'`
 homes=`cat $arquivoTeste | grep -A6  "\[homes\]" | grep -m1 "read only" | awk '{ print $NF }'`
 #echo "cat $arquivoTeste | grep -A6  "\[homes\]" | grep -m1 "read only" | awk '{ print $NF }'"
+
+verificando_servicos $1 $2 $3
