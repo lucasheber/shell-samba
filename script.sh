be9efaf75f804f4a40177640016b16b5fa5bc20a
Lucas Heber <lucas.heber07@gmail.com>
2019-12-02 00:30:36 -0300

    [up] verificando os serviços

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ UNIDADE_HOME="H"
 UNIDADE_ARQUIVOS="X"
 DIR_ARQUIVOS="/home/samba/arquivos"
 
-# Configuraçoes
+# Configuracoes
 workgroup=$1 # Nome do dominio via parametro
 arquivoTeste="testparm.txt"
 usersmb=$2
@@ -15,7 +15,7 @@ passuser=$3
 
 # Servidor Proxy
 pass="One8399*"
-proxyServer="192.168.1.130"
+proxyServer="192.168.11.11"
 proxyUser="root"
 
 # Servidor Samba
@@ -23,78 +23,81 @@ passSamba="One8399*"
 sambaServer="192.168.10.10"
 sambaUser="root"
 
-verificando_servicos()
-{
-    passSamba="One8399*"
-    sambaServer="192.168.10.10"
-    sambaUser="root"
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BGBLUE='\033[44m'
+NC='\033[0m' # No Color
 
-    pass="One8399*"
-    proxyServer="192.168.11.11"
-    proxyUser="root"
+# ################################################################################### #
+#                             VERIFICANDO OS SERVICOS                                 #
+# ################################################################################### #
 
-    work=$1
-    usersmb=$2
-    passuser=$3
+verificando_servicos() {
 
-    RED='\033[0;31m'
-    YELLOW='\033[1;33m'
-    NC='\033[0m' # No Color  
+    echo -e "\n                    ${BGBLUE} VERIFICANDO OS SERVICOS ${NC}\n"
 
-    # Configurações do samba
-    smbclient="smbclient -W $work -L 127.0.0.1 -U $usersmb%$passuser"
-    smbactive="systemctl is-active smb nmb" # Verificando se os servicos Samba estao ativos
+    # Configuracões do samba
+    smbclient="smbclient -W $workgroup -L 127.0.0.1 -U $usersmb%$passuser 2> /dev/null"
+    smbactive="systemctl is-active smb nmb"  # Verificando se os servicos Samba estao ativos
     smbenable="systemctl is-enabled smb nmb" # Verificando se os servicos Samba estao ativos
 
     smbclientSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbclient""
-    smbcliuser=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$smbclientSSH | grep $usersmb"`
+    smbcliuser=$(sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$smbclientSSH | grep $usersmb"  2> /dev/null)
 
     # Verificando se o smb e nmb estao ativos
     sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbactive""
-    smbactstatus=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH"`
+    smbactstatus=$(sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH")
 
     # Verificando se o smb e nmb estao "enabled"
     sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbenable""
-    smbenastatus=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH"`
+    smbenastatus=$(sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH")
 
-    result=`echo "$smbcliuser" | egrep "$NT_STATUS_LOGON_FAILURE|$NT_STATUS_CONNECTION_REFUSED" `
-    if [ "$result" != "" ]; then
-        echo -e "${RED}[ERROR]${NC} O usuario $usersmb nao conectou ao serviço smb"
+    result=$(echo "$smbcliuser" | grep "$NT_STATUS_LOGON_FAILURE|$NT_STATUS_CONNECTION_REFUSED" 2> /dev/null)
+
+    if [ -z "$result" -a "$smbcliuser" != "" ]; then
+        echo -e "${GREEN}[SUCCESS]${NC} O usuario $usersmb conectou ao servico smbclient!"
+    else
+        echo -e "${RED}[ERROR]${NC} O usuario $usersmb nao conectou ao servico smbclient!"
     fi
 
-    result=`echo "$smbactstatus" | sed -n '1p'`
+    result=$(echo "$smbactstatus" | sed -n '1p')
     if [ "$result" == "inactive" ]; then
-        echo -e "${YELLOW}[WARNING]${NC} O Serviço smb está inativo!"
+        echo -e "${YELLOW}[WARNING]${NC} O Servico smb esta inativo!"
+    else
+        echo -e "${GREEN}[SUCCESS]${NC} O Servico smb esta ativo!"
     fi
 
-    result=`echo "$smbactstatus" | sed -n '2p'`
+    result=$(echo "$smbactstatus" | sed -n '2p')
     if [ "$result" == "inactive" ]; then
-        echo -e "${YELLOW}[WARNING]${NC} O Serviço nmb está inativo!"
+        echo -e "${YELLOW}[WARNING]${NC} O Servico nmb esta inativo!"
+    else
+        echo -e "${GREEN}[SUCCESS]${NC} O Servico nmb esta ativo!"
     fi
 
-    result=`echo "$smbenastatus" | sed -n '1p'`
+    result=$(echo "$smbenastatus" | sed -n '1p')
     if [ "$result" == "disaled" ]; then
-        echo -e "${YELLOW}[WARNING]${NC} O Serviço smb nao está ativado para iniciar automaticamente!"
+        echo -e "${YELLOW}[WARNING]${NC} O Servico smb nao esta ativado para iniciar automaticamente!"
+    else
+        echo -e "${GREEN}[SUCCESS]${NC} O Servico smb esta ativado para iniciar automaticamente!"
     fi
 
-    result=`echo "$smbenastatus" | sed -n '2p'`
+    result=$(echo "$smbenastatus" | sed -n '2p')
     if [ "$result" == "disaled" ]; then
-        echo -e "${YELLOW}[WARNING]${NC} O Serviço nmb nao está ativado para iniciar automaticamente!"
+        echo -e "${YELLOW}[WARNING]${NC} O Servico nmb nao esta ativado para iniciar automaticamente!"
+    else
+        echo -e "${GREEN}[SUCCESS]${NC} O Servico nmb esta ativado para iniciar automaticamente!"
     fi
 }
-# ################################################################################### # 
+
+# ################################################################################### #
 #                          PEGANDO OS DADOS NECESSARIOS                               #
 # ################################################################################### #
 
-# Configurações do samba
-smbclient="smbclient -W $workgroup -L 127.0.0.1 -U $usersmb%$passuser"
-testparm="testparm -s"
-smbactive="systemctl is-active smb nmb" # Verificando se os servicos Samba estao ativos
-smbenable="systemctl is-enabled smb nmb" # Verificando se os servicos Samba estao ativos
 
-# Verificando o smbclient
-smbclientSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbclient""
-##smbcliuser=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$smbclientSSH | grep $usersmb"`
+# Configuracões do samba
+testparm="testparm -s"
 
 # Acessando o samba e executando o $testparm
 ##sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$testparm""
@@ -102,37 +105,34 @@ smbclientSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$
 # Acessando o proxy e obtendo o resultado do $testparm do servidor samba
 ## sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH" > $arquivoTeste 2> /dev/null
 
-
-
 # Pegando somente a primeira linha
-
 
 users="cat /etc/passwd | egrep 'pedrito|palito|palhaco'"
 ##userslinux=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$users"`
 ##echo $userslinux
 
-# ################################################################################### # 
+# ################################################################################### #
 #                          PEGANDO OS VALORES DO TESTPARM                             #
 # ################################################################################### #
 
 ## Pegando o os valores necessarios para PDC
-preferred=`cat $arquivoTeste | grep 'preferred master =' | awk '{ print $NF}'`
-domain=`cat $arquivoTeste | grep 'domain master =' | awk '{ print $NF}'`
-logons=`cat $arquivoTeste | grep 'domain logons =' | awk '{ print $NF}'`
-invaliduser=`cat $arquivoTeste | grep 'invalid users = root' | awk '{ print $NF}'`
-workgroup=`cat $arquivoTeste | grep 'workgroup =' | awk '{ print $NF}'`
-bindinterface=`cat $arquivoTeste | grep 'bind interfaces only =' | awk '{ print $NF }'`
-interfaces=`cat $arquivoTeste | grep 'interfaces =' | awk '{ print $(NF-1) " " $NF }'`
-security=`cat $arquivoTeste | grep 'security =' | awk '{ print $NF }'`
-wins=`cat $arquivoTeste | grep 'wins support =' | awk '{ print $NF }'`
+preferred=$(cat $arquivoTeste | grep 'preferred master =' | awk '{ print $NF}')
+domain=$(cat $arquivoTeste | grep 'domain master =' | awk '{ print $NF}')
+logons=$(cat $arquivoTeste | grep 'domain logons =' | awk '{ print $NF}')
+invaliduser=$(cat $arquivoTeste | grep 'invalid users = root' | awk '{ print $NF}')
+workgroup=$(cat $arquivoTeste | grep 'workgroup =' | awk '{ print $NF}')
+bindinterface=$(cat $arquivoTeste | grep 'bind interfaces only =' | awk '{ print $NF }')
+interfaces=$(cat $arquivoTeste | grep 'interfaces =' | awk '{ print $(NF-1) " " $NF }')
+security=$(cat $arquivoTeste | grep 'security =' | awk '{ print $NF }')
+wins=$(cat $arquivoTeste | grep 'wins support =' | awk '{ print $NF }')
 
-logondrive=`cat $arquivoTeste | grep 'logon drive =' | awk '{ print $NF }'`
-logonpath=`cat $arquivoTeste | grep 'logon path =' | awk '{ print $NF }'`
-logonscript=`cat $arquivoTeste | grep 'logon script =' | awk '{ print $NF }'`
-oslevel=`cat $arquivoTeste | grep 'os level =' | awk '{ print $NF }'`
-pathnetlogon=`cat $arquivoTeste | grep -A5 "\[netlogon\]" | grep -m1 "path" | awk '{ print $NF }'`
-crlf=`file $pathnetlogon/$logonscript | awk -F: '{ print $2}'`
-homes=`cat $arquivoTeste | grep -A6  "\[homes\]" | grep -m1 "read only" | awk '{ print $NF }'`
+logondrive=$(cat $arquivoTeste | grep 'logon drive =' | awk '{ print $NF }')
+logonpath=$(cat $arquivoTeste | grep 'logon path =' | awk '{ print $NF }')
+logonscript=$(cat $arquivoTeste | grep 'logon script =' | awk '{ print $NF }')
+oslevel=$(cat $arquivoTeste | grep 'os level =' | awk '{ print $NF }')
+pathnetlogon=$(cat $arquivoTeste | grep -A5 "\[netlogon\]" | grep -m1 "path" | awk '{ print $NF }')
+crlf=$(file $pathnetlogon/$logonscript | awk -F: '{ print $2}')
+homes=$(cat $arquivoTeste | grep -A6 "\[homes\]" | grep -m1 "read only" | awk '{ print $NF }')
 #echo "cat $arquivoTeste | grep -A6  "\[homes\]" | grep -m1 "read only" | awk '{ print $NF }'"
 
-verificando_servicos $1 $2 $3
+verificando_servicos
