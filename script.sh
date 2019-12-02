e7251b44491c3c62bf829a5a7d96294a4bbbc635
Lucas Heber <lucas.heber07@gmail.com>
2019-12-02 01:07:46 -0300

    [add] validando os dados

--- script.sh
+++ script.sh
@@ -30,6 +30,8 @@ YELLOW='\033[1;33m'
 BGBLUE='\033[44m'
 NC='\033[0m' # No Color
 
+eval userslinux=""
+
 # ################################################################################### #
 #                             VERIFICANDO OS SERVICOS                                 #
 # ################################################################################### #
@@ -44,7 +46,7 @@ verificando_servicos() {
     smbenable="systemctl is-enabled smb nmb" # Verificando se os servicos Samba estao ativos
 
     smbclientSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbclient""
-    smbcliuser=$(sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$smbclientSSH | grep $usersmb"  2> /dev/null)
+    smbcliuser=$(sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$smbclientSSH | grep $usersmb" 2>/dev/null)
 
     # Verificando se o smb e nmb estao ativos
     sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbactive""
@@ -54,7 +56,7 @@ verificando_servicos() {
     sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$smbenable""
     smbenastatus=$(sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH")
 
-    result=$(echo "$smbcliuser" | grep "$NT_STATUS_LOGON_FAILURE|$NT_STATUS_CONNECTION_REFUSED" 2> /dev/null)
+    result=$(echo "$smbcliuser" | grep "$NT_STATUS_LOGON_FAILURE|$NT_STATUS_CONNECTION_REFUSED" 2>/dev/null)
 
     if [ -z "$result" -a "$smbcliuser" != "" ]; then
         echo -e "${GREEN}[SUCCESS]${NC} O usuario $usersmb conectou ao servico smbclient!"
@@ -95,44 +97,76 @@ verificando_servicos() {
 #                          PEGANDO OS DADOS NECESSARIOS                               #
 # ################################################################################### #
 
+dados_necessarios() {
+    echo -e "\n                    ${BGBLUE} DADOS NECESSARIOS ${NC}\n"
 
-# Configuracões do samba
-testparm="testparm -s"
+    # Configuracões do samba
+    testparm="testparm -s"
 
-# Acessando o samba e executando o $testparm
-##sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$testparm""
+    # Acessando o samba e executando o $testparm
+    sambaSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$testparm""
 
-# Acessando o proxy e obtendo o resultado do $testparm do servidor samba
-## sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH" > $arquivoTeste 2> /dev/null
+    # Acessando o proxy e obtendo o resultado do $testparm do servidor samba
+    sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$sambaSSH" >$arquivoTeste 2>/dev/null
 
-# Pegando somente a primeira linha
+    users="cat /etc/passwd | egrep 'pedrito|palito|palhaco'"
+    usersSSH="sshpass -p $passSamba ssh -o StrictHostKeyChecking=no $sambaUser@$sambaServer "$users""
+    userslinux=$(sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$usersSSH")
 
-users="cat /etc/passwd | egrep 'pedrito|palito|palhaco'"
-##userslinux=`sshpass -p $pass ssh -o StrictHostKeyChecking=no $proxyUser@$proxyServer "$users"`
-##echo $userslinux
+    if [ -f $arquivoTeste ]; then
+        echo -e "${GREEN}[SUCCESS]${NC} Arquivo $arquivoTeste gerado!"
+        valida_dados
+    else
+        echo -e "${RED}[ERROR]${NC} Arquivo $arquivoTeste nao foi gerado!"
+    fi
+}
 
-# ################################################################################### #
-#                          PEGANDO OS VALORES DO TESTPARM                             #
-# ################################################################################### #
+valida_dados() {
+    echo -e "\n                    ${BGBLUE} VALIDANDO OS DADOS ${NC}\n"
 
-## Pegando o os valores necessarios para PDC
-preferred=$(cat $arquivoTeste | grep 'preferred master =' | awk '{ print $NF}')
-domain=$(cat $arquivoTeste | grep 'domain master =' | awk '{ print $NF}')
-logons=$(cat $arquivoTeste | grep 'domain logons =' | awk '{ print $NF}')
-invaliduser=$(cat $arquivoTeste | grep 'invalid users = root' | awk '{ print $NF}')
-workgroup=$(cat $arquivoTeste | grep 'workgroup =' | awk '{ print $NF}')
-bindinterface=$(cat $arquivoTeste | grep 'bind interfaces only =' | awk '{ print $NF }')
-interfaces=$(cat $arquivoTeste | grep 'interfaces =' | awk '{ print $(NF-1) " " $NF }')
-security=$(cat $arquivoTeste | grep 'security =' | awk '{ print $NF }')
-wins=$(cat $arquivoTeste | grep 'wins support =' | awk '{ print $NF }')
+    pontos=0
+    ## Pegando o os valores necessarios para PDC
+    preferred=$(cat $arquivoTeste | grep 'preferred master =' | awk '{ print $NF}')
+    domain=$(cat $arquivoTeste | grep 'domain master =' | awk '{ print $NF}')
+    logons=$(cat $arquivoTeste | grep 'domain logons =' | awk '{ print $NF}')
+    invaliduser=$(cat $arquivoTeste | grep 'invalid users = root' | awk '{ print $NF}')
+    workgroup=$(cat $arquivoTeste | grep 'workgroup =' | awk '{ print $NF}')
+    bindinterface=$(cat $arquivoTeste | grep 'bind interfaces only =' | awk '{ print $NF }')
+    interfaces=$(cat $arquivoTeste | grep 'interfaces =' | awk '{ print $(NF-1) " " $NF }')
+    security=$(cat $arquivoTeste | grep 'security =' | awk '{ print $NF }')
+    wins=$(cat $arquivoTeste | grep 'wins support =' | awk '{ print $NF }')
 
-logondrive=$(cat $arquivoTeste | grep 'logon drive =' | awk '{ print $NF }')
-logonpath=$(cat $arquivoTeste | grep 'logon path =' | awk '{ print $NF }')
-logonscript=$(cat $arquivoTeste | grep 'logon script =' | awk '{ print $NF }')
-oslevel=$(cat $arquivoTeste | grep 'os level =' | awk '{ print $NF }')
-pathnetlogon=$(cat $arquivoTeste | grep -A5 "\[netlogon\]" | grep -m1 "path" | awk '{ print $NF }')
-crlf=$(file $pathnetlogon/$logonscript | awk -F: '{ print $2}')
-homes=$(cat $arquivoTeste | grep -A6 "\[homes\]" | grep -m1 "read only" | awk '{ print $NF }')
-#echo "cat $arquivoTeste | grep -A6  "\[homes\]" | grep -m1 "read only" | awk '{ print $NF }'"
+    logondrive=$(cat $arquivoTeste | grep 'logon drive =' | awk '{ print $NF }')
+    logonpath=$(cat $arquivoTeste | grep 'logon path =' | awk '{ print $NF }')
+    logonscript=$(cat $arquivoTeste | grep 'logon script =' | awk '{ print $NF }')
+    oslevel=$(cat $arquivoTeste | grep 'os level =' | awk '{ print $NF }')
+    pathnetlogon=$(cat $arquivoTeste | grep -A5 "\[netlogon\]" | grep -m1 "path" | awk '{ print $NF }')
+    crlf=$(file $pathnetlogon/$logonscript | awk -F: '{ print $2}')
+    homes=$(cat $arquivoTeste | grep -A6 "\[homes\]" | grep -m1 "read only" | awk '{ print $NF }')
 
-verificando_servicos
+    ## VALIDANDO
+    if [ "$preferred" == "Yes" -a "$domain" == "Yes" -a "$logons" == "Yes" ]; then
+        echo -e "${GREEN}[SUCCESS]${NC} Configurado para PDC. (+3 Pontos)"
+        pontos=`expr $pontos + 3`
+    else
+        echo -e "${RED}[ERROR]${NC} Nao configurado para PDC. (+0 Pontos)"
+    fi
+
+    if [ "$bindinterface" == "Yes" -a "$interfaces" == "lo enp0s8" -o "$interfaces" == "enp0s8 lo"  ]; then
+        echo -e "${GREEN}[SUCCESS]${NC} Configurado para somente a rede local ter acesso. (+3 Pontos)"
+        pontos=`expr $pontos + 3`
+    else
+        echo -e "${RED}[ERROR]${NC} Nao configurado para somente a rede local ter acesso. (+0 Pontos)"
+    fi
+
+    # Invalid User
+    if [ ! -z $invaliduser ]; then
+        echo -e "${YELLOW}[WARNING]${NC} O usuario root esta configuradao para fazer logon!"
+    fi
+
+    echo "Total: $pontos pontos";
+}
+
+#verificando_servicos
+dados_necessarios
+#rm -f $arquivoTeste
